4773eb4817bf028f2b25367dc7926b3f7b903dcb
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-04-25 11:22:12 +0100

    added

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req,res) => {
+    res.send("hello World");
+});
+
+
+app.get("/api/courses", (req,res) => {
+    res.send([1,2,3]);
+});
+
+// /api/courses/1
+// define the paramater , id is the name of the paramater 
+
+app.get("/api/courses/:id", (req,res) =>{
+    res.send (req.params.id);
+});
+
+//Port
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
