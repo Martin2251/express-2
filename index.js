8568cd129c55fc76f062212cfc50349d15ce4b69
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-04-25 12:29:54 +0100

    validation logic before joi

--- index.js
+++ index.js
@@ -1,20 +1,52 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+const courses = [
+    {id: 1, name: "course1"},
+    {id: 2, name: "course2"},
+    {id: 3, name: "course3"},
+];
+
+
+
 app.get("/", (req,res) => {
     res.send("hello World");
 });
 
 
 app.get("/api/courses", (req,res) => {
-    res.send([1,2,3]);
+    res.send(courses);
 });
 
 // /api/courses/1
 // define the paramater , id is the name of the paramater 
 
 app.get("/api/courses/:id", (req,res) =>{
-    res.send (req.params.id);
+const course = courses.find(c => c.id === parseInt(req.params.id));
+if(!course) res.status(404).send("The courses is not found!") //404 object not found standard convention 
+res.send(course);
+});
+
+app.get("/api/posts/:year/:month", (req, res) => {
+    res.send(req.params);
+});
+
+
+app.post("/api/courses", (req, res) => {
+    if(!req.body.name || req.body.name.length < 3){
+        // 400 bad request
+        res.status(400).send("Name is required ")
+        return;
+    }
+   const course = {
+id: courses.length +1,
+name: req.body.name
+   
+   };
+   courses.push(course);
+   res.send(course);
 });
 
 //Port hh
